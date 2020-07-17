f04a12ff5dd6659171fe6b12249759190c4edfcc
lcano2015 <canomartinezluis@gmail.com>
2020-07-17 02:30:24 -0500

    Luis Cano

---
+++ SGRS/Controllers/ReporteController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SGRS.Controllers
+{
+    public class ReporteController : Controller
+    {
+        public ActionResult ReporteIndicador()
+        {
+            return PartialView();
+        }
+        public ActionResult ReporteGasto()
+        {
+            return PartialView();
+        }
+        public ActionResult ReporteRequerimiento()
+        {
+            return PartialView();
+        }
+
+      
+
+
+
+
+    }
+}
---
+++ SGRS/Js/DataTableSGRS.js
@@ -0,0 +1,27 @@
+﻿
+(function ($) {
+
+    'use strict';
+
+    $("#datatable").DataTable({
+        "language": {
+            "paginate": {
+                "previous": "anterior",
+                "next": "posterior"
+            },
+            "search": "Buscar:",
+            "info": "Total de registros _TOTAL_",
+            "infoEmpty": "0 Registros",
+            "loadingRecords": "Cargando...",
+            "processing": "Procesando...",
+            "emptyTable": "No tiene ningún registro",
+            "lengthMenu": "Muestra _MENU_ Registros"
+        },
+        "search": "Buscar:",
+        "lengthMenu": [[5, 10, 25, -1], [5, 10, 25, "All"]]
+
+    });
+
+
+}).apply(this, [jQuery]);
+
